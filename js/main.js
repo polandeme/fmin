10ec3e8f0177a8dfe37cf891b3d3dc2cc6c909e7
polande <polandeme@gmail.com>
2014-07-13 17:17:34 +0800

    array

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var row_table = new Array(),
     num_j = new Array();
     cell = $("td");
+
 for(var i = 0; i < cell.length; i++) {
     row_table.push(cell.eq(i));
 }
-
 
 function init(num) {
     j = Math.round(Math.random() * (row_table.length-1));
@@ -12,33 +12,46 @@ function init(num) {
         while(row_table[j] === undefined) {
             j = Math.round(Math.random() * (row_table.length-1));
         }
-            row_table[j].addClass("red");//css("background","red");
+            row_table[j].addClass("red");
             var cell_num = "<div class = 'cell-num'>" + j + "</div>";
             num_j.push(j);
-            // console.log(num_j.length);
             row_table[j].html(cell_num);
             delete row_table[j];
     }
 }
+
 init(2);
-cell.click(function() {
+
+$("body").on("click", ".red" ,function() {
     var this_num = parseInt($(this).text()),
         self = $(this);
     if(this_num > _.min(num_j) ) {
-        console.log("f");
         self.addClass("blue")
         setTimeout(function(){
-            self.removeClass().addClass("red");//css("background", "red");
+            self.removeClass().addClass("red");
         },50);
     } else {
-        self.removeClass().addClass("while").text('');
-        num_j.splice(self.index(), 1);
-        // console.log(_.min(num_j);
-        console.log(_.min(num_j));
-        console.log(num_j[0]);
-        console.log(num_j[1]);
-        console.log(_.min(num_j));
+        self.removeClass().addClass("white").text('');
+        num_j.remove(_.min(num_j));
+        // num_j.splice(self.index(), 1);
         init(1);
-
     }
-})
+});
+
+//delete element from array by element
+Array.prototype.indexOf = function(val) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === val) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
+};
+
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if( index > -1 ) {
+        this.splice(index,1);
+    }
+};
