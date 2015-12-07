af4064f262be9cd54a5c111d696c21c565474269
allen-blue <polandeme@gmail.com>
2015-12-07 20:02:57 +0800

    add base module

---
+++ js/base.js
@@ -0,0 +1,21 @@
+// var Cell = {
+// 	i,
+// 	j,
+// 	value,
+// 	available
+// }
+
+function initCell() {
+	
+}
+/**
+ * 1. 多个对象push进去
+ * 2. 增加attr, 点击后改变当前attr,获取新的attr.
+ * 3. 
+ */
+
+ /*
+  * 函数名驼峰
+  * 变量名 下划线
+  * 
+  */
