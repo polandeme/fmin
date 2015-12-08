767a6d6ec58a117b9b99e07262d838ecd7f54148
polandeme <polandeme@gmail.com>
2015-12-08 23:10:16 +0800

    init cell by js

--- js/base.js
+++ js/base.js
@@ -4,15 +4,22 @@
 // 	value,
 // 	available
 // }
-
+//初始化cell
 function initCell(n) {
 	var list = '';
 	var li = '';
 	for(var i = 0; i < n; i++) {
-		li = '<li class="cell-item">' + '</li>';
+		if(i <= 11) {
+			li = '<li class="cell-item border-un-bottom">' + '</li>';
+		} else {
+			li = '<li class="cell-item border-bottom">' + '</li>';
+		}
 		list += li;
 	}
+	$('.cell-group').html(list);
 }
+
+initCell(16);
 /**
  * 1. 多个对象push进去
  * 2. 增加attr, 点击后改变当前attr,获取新的attr.
