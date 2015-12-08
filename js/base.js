fae9a2a387ffa8af893d319a5b9d3e5362b75daf
polandeme <polandeme@gmail.com>
2015-12-09 00:04:02 +0800

    insert num init

--- js/base.js
+++ js/base.js
@@ -19,7 +19,33 @@ function initCell(n) {
 	$('.cell-group').html(list);
 }
 
+//
+function getRandomNum(base) {
+	var base = base || 10;
+	var num_one = Math.floor(Math.random() * base);
+	var num_two = Math.floor(Math.random() * base);
+	while(num_one === num_two) {
+		num_two = Math.floor(Math.random() * base);
+	}
+	return {
+		num_one: num_one,
+		num_two: num_two
+	}
+}
+
 initCell(16);
+insertNum();
+function insertNum() {
+	var index_one = Math.floor(Math.random() * 16);
+	var index_two = Math.floor(Math.random() * 16);
+	while(index_two === index_one) {
+		index_two = Math.floor(Math.random() * 16);
+	}
+	var randomNum = getRandomNum();
+	$('.cell-item').eq(index_one).html('<span>' + randomNum.num_one + '</span>');
+	$('.cell-item').eq(index_two).html('<span>' + randomNum.num_two + '</span>');
+}
+
 /**
  * 1. 多个对象push进去
  * 2. 增加attr, 点击后改变当前attr,获取新的attr.
